1e33d059837913b5408dddc3bf479cf4fed9d082
Hung Nguyen <hung.nguyen@sentifi.com>
2017-08-08 16:56:28 +0700

    refactor code

--- src/components/app.js
+++ src/components/app.js
@@ -1,134 +1,53 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { Field, reduxForm } from 'redux-form'
-import { load as loadAccount } from '../actions/index';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Field, reduxForm } from 'redux-form';
+import { load } from '../actions/index';
 
 const data = {
-  // used to populate "account" reducer when "Load" is clicked
-  firstName: 'Jane',
-  lastName: 'Doe',
-  age: '42',
-  sex: 'female',
-  employed: true,
-  favoriteColor: 'Blue',
-  bio: 'Born to write amazing Redux code.'
-}
-const colors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
-
-let InitializeFromStateForm = props => {
-  const { handleSubmit, load, pristine, reset, submitting } = props
-  console.log('PROPS', props);
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <button type="button" onClick={() => load(data)}>
-          Load Account
-        </button>
-      </div>
-      <div>
-        <label>First Name</label>
-        <div>
-          <Field
-            name="firstName"
-            component="input"
-            type="text"
-            placeholder="First Name"
-          />
-        </div>
-      </div>
-      <div>
-        <label>Last Name</label>
-        <div>
-          <Field
-            name="lastName"
-            component="input"
-            type="text"
-            placeholder="Last Name"
-          />
-        </div>
-      </div>
-      <div>
-        <label>Age</label>
-        <div>
-          <Field name="age" component="input" type="number" placeholder="Age" />
-        </div>
-      </div>
-      <div>
-        <label>Sex</label>
-        <div>
-          <label>
-            <Field
-              name="sex"
-              component="input"
-              type="radio"
-              value="male"
-            />{' '}
-            Male
-          </label>
-          <label>
-            <Field
-              name="sex"
-              component="input"
-              type="radio"
-              value="female"
-            />{' '}
-            Female
-          </label>
-        </div>
-      </div>
-      <div>
-        <label>Favorite Color</label>
-        <div>
-          <Field name="favoriteColor" component="select">
-            <option value="">Select a color...</option>
-            {colors.map(colorOption =>
-              <option value={colorOption} key={colorOption}>
-                {colorOption}
-              </option>
-            )}
-          </Field>
-        </div>
-      </div>
-      <div>
-        <label htmlFor="employed">Employed</label>
-        <div>
-          <Field
-            name="employed"
-            id="employed"
-            component="input"
-            type="checkbox"
-          />
-        </div>
-      </div>
-      <div>
-        <label>Bio</label>
-        <div>
-          <Field name="bio" component="textarea" />
-        </div>
-      </div>
-      <div>
-        <button type="submit" disabled={pristine || submitting}>
-          Submit
-        </button>
-        <button type="button" disabled={pristine || submitting} onClick={reset}>
-          Undo Changes
-        </button>
-      </div>
-    </form>
-  )
+  search: 'Hello'
 }
 
-// Decorate with reduxForm(). It will read the initialValues prop provided by connect()
-InitializeFromStateForm = reduxForm({
-  form: 'initializeFromState' // a unique identifier for this form
-})(InitializeFromStateForm)
+class App extends Component {
+  onFormSubmit(event) {
+    console.log('EVENT', event);
+  }
 
-// You have to connect() to any reducers that you wish to connect to yourself
-InitializeFromStateForm = connect(
-  state => ({
-    initialValues: state.load.data // pull initial values from account reducer
-  }),
-  { load: loadAccount } // bind account loading action creator
-)(InitializeFromStateForm)
+  render() {
+    console.log('PROPS', this.props)
+     const { handleSubmit, load, reset, pristine, submitting } = this.props;
+    return (
+      <div className="form-search">
+        <form className="input-group" onSubmit={handleSubmit(this.onFormSubmit)}>
+          <Field
+            name="search"
+            type="text"
+            className="form-control"
+            placeholder="Enter location"
+            component="input"
+          />
+          <span className="input-group-btn">
+            <button tyep="button" onClick={() => load(data)} className="btn btn-secondary">Search</button>
+            <button type="button" className="btn btn-secondary" disabled={pristine || submitting} onClick={reset}>
+          Clear Values
+        </button>
+          </span>
+        </form>
+      </div>
+    );
+  }
+}
 
-export default InitializeFromStateForm
+const mapStateToProps = (state) => {
+  return {
+    initialValues: state.load
+  }
+}
+
+const mapDispatchTOProps = (dispatch) => {
+  return bindActionCreators({ load }, dispatch);
+}
+
+export default reduxForm({
+  form: 'clearForm'
+})(connect(mapStateToProps, mapDispatchTOProps)(App));
